98e8e992d5b1172d3d9f6903c1f892b79c9756c3
Rommel <rommel.cc.lo@gmail.com>
2020-08-11 09:31:30 -0500

    hola

---
+++ lab-6/app/src/main/java/com/example/examen1/MainActivity.java
@@ -0,0 +1,140 @@
+package com.example.examen1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+        private EditText et_codigo,et_descripcion,et_precio;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        et_codigo=(EditText)findViewById(R.id.txt_codigo);
+        et_descripcion=(EditText)findViewById(R.id.txt_descripcion);
+        et_precio=(EditText)findViewById(R.id.txt_precio);
+
+    }
+
+    //metodo para dar de alta a los productos
+    public void Registrar(View view){
+        AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(this,"administracion",null,1);
+        SQLiteDatabase BaseDeDatos = admin.getWritableDatabase();
+
+        String codigo=et_codigo.getText().toString();
+        String descripcion=et_descripcion.getText().toString();
+        String precio=et_precio.getText().toString();
+
+        if(!codigo.isEmpty() && !descripcion.isEmpty() && !precio.isEmpty()) {
+            ContentValues registro = new ContentValues();
+            registro.put("codigo", codigo);
+            registro.put("descripcion", descripcion);
+            registro.put("precio", precio);
+
+            BaseDeDatos.insert("articulos", null, registro);
+
+            BaseDeDatos.close();
+            et_codigo.setText("");
+            et_descripcion.setText("");
+            et_precio.setText("");
+        }
+        else {
+            Toast.makeText(this,"Debe llenar todos los campos",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    //metodo para consultar
+    public void  Buscar(View view){
+        AdminSQLiteOpenHelper admin= new AdminSQLiteOpenHelper(this,"administracion",null,1);
+        SQLiteDatabase BaseDeDatabase = admin.getWritableDatabase();
+
+        String codigo = et_codigo.getText().toString();
+
+        if(!codigo.isEmpty()){
+            Cursor fila =BaseDeDatabase.rawQuery
+                    ("select descripcion,precio from articulos where codigo =" +codigo,null);
+
+        if(fila.moveToFirst()){
+            et_descripcion.setText(fila.getString(0));
+            et_precio.setText(fila.getString(1));
+            BaseDeDatabase.close();
+
+        }else{
+            Toast.makeText(this,"No existe el articulo",Toast.LENGTH_SHORT).show();
+            BaseDeDatabase.close();
+        }
+
+        }else{
+            Toast.makeText(this,"Debe llenar codigo del articulo",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+//metodo para eliminar  producto
+//Método para eliminar un producto o Artículo
+public void Eliminar(View view){
+    AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper
+            (this, "administracion", null, 1);
+    SQLiteDatabase BaseDatabase = admin.getWritableDatabase();
+
+    String codigo = et_codigo.getText().toString();
+
+    if(!codigo.isEmpty()){
+
+        int cantidad = BaseDatabase.delete("articulos", "codigo=" + codigo, null);
+        BaseDatabase.close();
+
+        et_codigo.setText("");
+        et_descripcion.setText("");
+        et_precio.setText("");
+
+        if(cantidad == 1){
+            Toast.makeText(this, "Artículo eliminado exitosamente", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "El artículo no existe", Toast.LENGTH_SHORT).show();
+        }
+
+    } else {
+        Toast.makeText(this, "Debes de introducir el código del artículo", Toast.LENGTH_SHORT).show();
+    }
+}
+
+
+    //Método para modificar un artículo o producto
+    public void Modificar(View view){
+        AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(this, "administracion", null, 1);
+        SQLiteDatabase BaseDatabase = admin.getWritableDatabase();
+
+        String codigo = et_codigo.getText().toString();
+        String descripcion = et_descripcion.getText().toString();
+        String precio = et_precio.getText().toString();
+
+        if(!codigo.isEmpty() && !descripcion.isEmpty() && !precio.isEmpty()){
+
+            ContentValues registro = new ContentValues();
+            registro.put("codigo", codigo);
+            registro.put("descripcion", descripcion);
+            registro.put("precio", precio);
+
+            int cantidad = BaseDatabase.update("articulos", registro, "codigo=" + codigo, null);
+            BaseDatabase.close();
+
+            if(cantidad == 1){
+                Toast.makeText(this, "Artículo modificado correctamente", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "El artículo no existe", Toast.LENGTH_SHORT).show();
+            }
+
+        } else {
+            Toast.makeText(this, "Debes llenar todos los campos", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
